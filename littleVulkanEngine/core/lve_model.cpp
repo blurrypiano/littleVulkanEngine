796cba53d14726dd26bb9a508c1163a3f6545f54
Brendan Galea <blurrypiano@gmail.com>
2020-11-24 10:47:16 -0500

    Check index bounds when loading model

--- littleVulkanEngine/core/lve_model.cpp
+++ littleVulkanEngine/core/lve_model.cpp
@@ -173,21 +173,28 @@ void LveModel::Builder::loadModel(std::string filepath) {
     int count = 0;
     for (const auto& index : shape.mesh.indices) {
       Vertex vertex{};
-      vertex.position = {
-          attrib.vertices[3 * index.vertex_index + 0],
-          attrib.vertices[3 * index.vertex_index + 1],
-          attrib.vertices[3 * index.vertex_index + 2]};
 
-      vertex.uv = {
-          attrib.texcoords[2 * index.texcoord_index + 0],
-          1.0f - attrib.texcoords[2 * index.texcoord_index + 1]};
+      if (index.vertex_index >= 0) {
+        vertex.position = {
+            attrib.vertices[3 * index.vertex_index + 0],
+            attrib.vertices[3 * index.vertex_index + 1],
+            attrib.vertices[3 * index.vertex_index + 2]};
+      }
+
+      if (index.texcoord_index >= 0) {
+        vertex.uv = {
+            attrib.texcoords[2 * index.texcoord_index + 0],
+            1.0f - attrib.texcoords[2 * index.texcoord_index + 1]};
+      }
 
       vertex.color = {1.0f, 1.0f, 1.0f, 1.0f};
 
-      vertex.normal = {
-          attrib.normals[3 * index.normal_index + 0],
-          attrib.normals[3 * index.normal_index + 1],
-          attrib.normals[3 * index.normal_index + 2]};
+      if (index.normal_index >= 0) {
+        vertex.normal = {
+            attrib.normals[3 * index.normal_index + 0],
+            attrib.normals[3 * index.normal_index + 1],
+            attrib.normals[3 * index.normal_index + 2]};
+      }
 
       if (uniqueVertices.count(vertex) == 0) {
         uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
