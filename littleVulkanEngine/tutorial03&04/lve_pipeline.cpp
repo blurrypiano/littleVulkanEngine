494ef6d28b3443699f4785ed4038d329f5fdbf5d
Brendan Galea <blurrypiano@gmail.com>
2021-01-18 07:59:07 -0500

    Add comment about issue

--- littleVulkanEngine/tutorial03&04/lve_pipeline.cpp
+++ littleVulkanEngine/tutorial03&04/lve_pipeline.cpp
@@ -144,6 +144,7 @@ PipelineConfigInfo LvePipeline::defaultPipelineConfigInfo(uint32_t width, uint32
   configInfo.scissor.offset = {0, 0};
   configInfo.scissor.extent = {width, height};
 
+  // Known issue: this creates a self-referencing structure. Fixed in tutorial 05
   configInfo.viewportInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
   configInfo.viewportInfo.viewportCount = 1;
   configInfo.viewportInfo.pViewports = &configInfo.viewport;
