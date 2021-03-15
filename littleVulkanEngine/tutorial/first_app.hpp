c845aa519fa86ba71c07ac33c9191582e2b2601d
brendan <blurrypiano@gmail.com>
2021-03-15 12:44:51 -0400

    video continuity fix

--- littleVulkanEngine/tutorial/first_app.hpp
+++ littleVulkanEngine/tutorial/first_app.hpp
@@ -30,10 +30,12 @@ class FirstApp {
   void createPipeline();
   void createCommandBuffers();
   void drawFrame();
+  void recreateSwapChain();
+  void recordCommandBuffer(int imageIndex);
 
   LveWindow lveWindow{WIDTH, HEIGHT, "Tutorial 07"};
   LveDevice lveDevice{lveWindow};
-  LveSwapChain lveSwapChain{lveDevice, lveWindow.getExtent()};
+  std::unique_ptr<LveSwapChain> lveSwapChain;
   std::unique_ptr<LvePipeline> lvePipeline;
   VkPipelineLayout pipelineLayout;
   std::vector<VkCommandBuffer> commandBuffers;
