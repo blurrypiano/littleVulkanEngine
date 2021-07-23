411336151b35c9434b0df73fd4f0f94febd99589
brendan <blurrypiano@gmail.com>
2021-07-22 20:03:28 -0400

    Tutorial 16 changes

--- littleVulkanEngine/tutorial/first_app.cpp
+++ littleVulkanEngine/tutorial/first_app.cpp
@@ -57,63 +57,54 @@ void FirstApp::run() {
   vkDeviceWaitIdle(lveDevice.device());
 }
 
-// temporary helper function, creates a 1x1x1 cube centered at offset
+// temporary helper function, creates a 1x1x1 cube centered at offset with an index buffer
 std::unique_ptr<LveModel> createCubeModel(LveDevice& device, glm::vec3 offset) {
-  std::vector<LveModel::Vertex> vertices{
-
+  LveModel::Builder modelBuilder{};
+  modelBuilder.vertices = {
       // left face (white)
       {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
       {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
       {{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
-      {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
       {{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
-      {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
 
       // right face (yellow)
       {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
       {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
       {{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
-      {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
       {{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
-      {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
 
       // top face (orange, remember y axis points down)
       {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
       {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
       {{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
-      {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
       {{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
-      {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
 
       // bottom face (red)
       {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
       {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
       {{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
-      {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
       {{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
-      {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
 
       // nose face (blue)
       {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
       {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
       {{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
-      {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
       {{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
-      {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
 
       // tail face (green)
       {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
       {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
       {{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
-      {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
       {{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
-      {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
-
   };
-  for (auto& v : vertices) {
+  for (auto& v : modelBuilder.vertices) {
     v.position += offset;
   }
-  return std::make_unique<LveModel>(device, vertices);
+
+  modelBuilder.indices = {0,  1,  2,  0,  3,  1,  4,  5,  6,  4,  7,  5,  8,  9,  10, 8,  11, 9,
+                          12, 13, 14, 12, 15, 13, 16, 17, 18, 16, 19, 17, 20, 21, 22, 20, 23, 21};
+
+  return std::make_unique<LveModel>(device, modelBuilder);
 }
 
 void FirstApp::loadGameObjects() {
--- littleVulkanEngine/tutorial/lve_model.hpp
+++ littleVulkanEngine/tutorial/lve_model.hpp
@@ -21,7 +21,12 @@ class LveModel {
     static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
   };
 
-  LveModel(LveDevice &device, const std::vector<Vertex> &vertices);
+  struct Builder {
+    std::vector<Vertex> vertices{};
+    std::vector<uint32_t> indices{};
+  };
+
+  LveModel(LveDevice &device, const LveModel::Builder &builder);
   ~LveModel();
 
   LveModel(const LveModel &) = delete;
@@ -32,10 +37,17 @@ class LveModel {
 
  private:
   void createVertexBuffers(const std::vector<Vertex> &vertices);
+  void createIndexBuffers(const std::vector<uint32_t> &indices);
 
   LveDevice &lveDevice;
+
   VkBuffer vertexBuffer;
   VkDeviceMemory vertexBufferMemory;
   uint32_t vertexCount;
+
+  bool hasIndexBuffer = false;
+  VkBuffer indexBuffer;
+  VkDeviceMemory indexBufferMemory;
+  uint32_t indexCount;
 };
 }  // namespace lve
