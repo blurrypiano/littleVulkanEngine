37538ba18f41bbecb5b68ccf1a88230f19857033
brendan <blurrypiano@gmail.com>
2021-10-18 20:32:58 -0400

    update tutorial20 starting point with bug fix

--- littleVulkanEngine/tutorial/first_app.cpp
+++ littleVulkanEngine/tutorial/first_app.cpp
@@ -29,15 +29,16 @@ FirstApp::FirstApp() { loadGameObjects(); }
 FirstApp::~FirstApp() {}
 
 void FirstApp::run() {
-  LveBuffer globalUboBuffer{
-      lveDevice,
-      sizeof(GlobalUbo),
-      LveSwapChain::MAX_FRAMES_IN_FLIGHT,
-      VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
-      VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT,
-      lveDevice.properties.limits.minUniformBufferOffsetAlignment,
-  };
-  globalUboBuffer.map();
+ std::vector<std::unique_ptr<LveBuffer>> uboBuffers(LveSwapChain::MAX_FRAMES_IN_FLIGHT);
+  for (int i = 0; i < uboBuffers.size(); i++) {
+    uboBuffers[i] = std::make_unique<LveBuffer>(
+        lveDevice,
+        sizeof(GlobalUbo),
+        1,
+        VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT);
+    uboBuffers[i]->map();
+  }
 
   SimpleRenderSystem simpleRenderSystem{lveDevice, lveRenderer.getSwapChainRenderPass()};
   LveCamera camera{};
@@ -67,8 +68,8 @@ void FirstApp::run() {
       // update
       GlobalUbo ubo{};
       ubo.projectionView = camera.getProjection() * camera.getView();
-      globalUboBuffer.writeToIndex(&ubo, frameIndex);
-      globalUboBuffer.flushIndex(frameIndex);
+      uboBuffers[frameIndex]->writeToBuffer(&ubo);
+      uboBuffers[frameIndex]->flush();
 
       // render
       lveRenderer.beginSwapChainRenderPass(commandBuffer);
