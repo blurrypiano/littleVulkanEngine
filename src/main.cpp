dd2b4cdceebbcdef492d87f1a6e59b3cd18c3278
Nitin Gupta <nitingupta910@gmail.com>
2021-12-22 22:47:33 -0800

    Add tutorial 0
    Dev setup test

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,34 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
-#include "first_app.hpp"
-
-// std
-#include <cstdlib>
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
 #include <iostream>
-#include <stdexcept>
 
 int main() {
-  lve::FirstApp app{};
+  glfwInit();
 
-  try {
-    app.run();
-  } catch (const std::exception &e) {
-    std::cerr << e.what() << '\n';
-    return EXIT_FAILURE;
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+  uint32_t extensionCount = 0;
+  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+  std::cout << extensionCount << " extensions supported\n";
+
+  glm::mat4 matrix;
+  glm::vec4 vec;
+  auto test = matrix * vec;
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
   }
 
-  return EXIT_SUCCESS;
+  glfwDestroyWindow(window);
+
+  glfwTerminate();
+
+  return 0;
 }
